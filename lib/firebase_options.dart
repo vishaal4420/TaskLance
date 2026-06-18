7977bfa4cc1df66df85f63750c1cc1f3cb31ee02
TaskLance Developer <dev@tasklance.com>
2026-06-18 14:44:23 +0530

    Secure API keys using flutter_dotenv and remove google-services.json from tracking

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
@@ -17,8 +18,8 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: '[REDACTED]',
+  static FirebaseOptions get web => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_WEB'] ?? '',
     appId: '1:848710174872:web:efa883ec3ea4bd7037e8ed',
     messagingSenderId: '848710174872',
     projectId: 'tasklance-579d3',
@@ -27,8 +28,8 @@ class DefaultFirebaseOptions {
     measurementId: 'G-17FDK7DWLC',
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: '[REDACTED]',
+  static FirebaseOptions get android => FirebaseOptions(
+    apiKey: dotenv.env['FIREBASE_API_KEY_ANDROID'] ?? '',
     appId: '1:848710174872:android:fec553aa7c79db6c37e8ed',
     messagingSenderId: '848710174872',
     projectId: 'tasklance-579d3',
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'app.dart';
@@ -10,11 +11,13 @@ import 'firebase_options.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 }
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
 
   // Run Firebase and Hive initializations in parallel to reduce startup time
   await Future.wait([
